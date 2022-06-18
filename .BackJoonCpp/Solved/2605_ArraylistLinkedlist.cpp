ba03593a3d8276338996e95c1c338cb522c3cc6e
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-06-19 02:13:17 +0900

    sol 2605

--- .BackJoonCpp/Solved/2605_ArraylistLinkedlist.cpp
+++ .BackJoonCpp/Solved/2605_ArraylistLinkedlist.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <stdio.h>
 
 using namespace std;
 
@@ -10,23 +9,23 @@ int main()
     
     vector<int> student_n;
     int n, n_temp;
-    cin >> n;
+    scanf("%d", &n);
     for (int i=0; i<n; i++)
     {
-        cin >> n_temp;
+        scanf("%d", &n_temp);
         int student = i+1;
         
         if(n_temp == 0) {student_n.push_back(student);}
         else 
         {
-            int index = n - n_temp - 1;
-            student_n.(student_n.begin() + index, student);
+            int index = student -1 - n_temp;
+            student_n.emplace(student_n.begin() + index, student);
         }
     }
     
     for (vector<int>::iterator it=student_n.begin(); it!=student_n.end(); it++)
     {
-      cout << *it << " ";
+      printf("%d ",*it);
     }
     cout << endl;
 }
