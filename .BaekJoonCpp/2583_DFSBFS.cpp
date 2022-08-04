eb7e93880defc6d48087d22a85f6485ce21ec1b2
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2022-08-05 01:10:45 +0900

    update

--- .BaekJoonCpp/2583_DFSBFS.cpp
+++ .BaekJoonCpp/2583_DFSBFS.cpp
@@ -1,13 +1,54 @@
 #include <iostream>
+#include <vector>
+#include<utility>
+#include <algorithm>
+#define MAX 100
 using namespace std;
-# define
-int M, N, K;
 
+int map[MAX][MAX];
+int visit[MAX][MAX];
+
+int M, N, K;
+pair<int, int> check_pair[4] = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
+int DFS(int y,int x);
 int main() {
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
     cin >> M >> N >> K;
     int le_do_x, le_do_y, ri_up_x, ri_up_y; 
+    vector<int> visited_cell;
+    
+    // draw map
     for (int i=0; i<K; i++) {
         cin >> le_do_x >> le_do_y >> ri_up_x >> ri_up_y;
-
+        for(int j=le_do_x; j<ri_up_x; j++) {for(int k=le_do_y; k<ri_up_y; k++) {map[k][j]=1;}}
     }
+    for(int i=0;i<M;i++){
+        for(int j=0; j<N; j++){
+            if((!visit[i][j])&&(!map[i][j])){
+                visit[i][j]=1;
+                visited_cell.push_back(DFS(i,j));
+            }
+        }
+    }
+    cout << visited_cell.size()<< endl;
+    sort(visited_cell.begin(),visited_cell.end());
+    for(int i=0;i<visited_cell.size();i++) {cout << visited_cell[i] << " ";}
+    cout << endl;
 }
+
+int DFS(int y,int x){
+    int count = 1;
+    for(int l=0; l<4; l++){
+        int test_x = x + check_pair[l].second;
+        int test_y = y + check_pair[l].first;
+        
+        if ((visit[test_y][test_x])||(map[test_y][test_x])) {continue;}
+        else if((test_x<0)||(test_x>=N)||(test_y<0)||(test_y>=M)) {continue;}
+        else {        
+            visit[test_y][test_x] = 1;
+            count += DFS(test_y,test_x);
+        }
+    }
+    return count;
+} 
--- .BaekJoonCpp/4195_unionFindHashing.cpp
+++ .BaekJoonCpp/4195_unionFindHashing.cpp
@@ -1,59 +1,57 @@
 #include <iostream>
-#include <algorithm>
 #include <map>
+#include <set>
+# define MAX_SIZE 200002
+
 using namespace std;
-int testcase, relation_n;
-string name1, name2;
 
-int main ()
-{
-    scanf("%d", &testcase);
-    for (int i=0; i<testcase; i++) {
-        map<string, string> relation;
-        scanf("%d", &relation_n);
-        for (int j=0; j<relation_n; j++) {
-            cin >> name1 >> name2;
-            auto  itroot1 = relation.end(), itroot2 = relation.end();
-            bool it1_ex=false, it2_ex=false;
-            int friend_n = 0;
+int sizes[MAX_SIZE];
+int link[MAX_SIZE];
 
-            // find root if exist
-            for (auto it = relation.begin(); it != relation.end(); ++it) {if (it->second == name1) { itroot1 = it; break;}}
-            for (auto it = relation.begin(); it != relation.end(); ++it) {if (it->second == name2) { itroot2 = it; break;}}
-            if (itroot1 != relation.end()) {it1_ex = true;}
-            if (itroot2 != relation.end()) {it2_ex = true;}
-            // two exist 
-            if ((it1_ex)&&(it2_ex)) {
-                if (itroot1->first == itroot2->first) {
-                    friend_n = relation.count(itroot1->first);
-                } else {
-                    string itroot2_name = itroot2->first;
-                    for (auto iter = relation.begin(); iter != relation.end(); iter++) {
-                        if (iter->first == itroot2_name) {
-                            relation.insert({itroot1->first, iter->second});
-                            relation.erase(iter++);
-                        }
-                    }
-                    friend_n = relation.count(itroot1->first);
-                }
-            }
-            else if(it1_ex) {
-                relation.insert({itroot1->first, name2});
-                friend_n = relation.count(itroot1->first);
+int find_root(int x) {
+	if (link[x] == x) return x;
+	return link[x] = find_root(link[x]);
+}
 
-            }
-            else if(it2_ex) {
-                relation.insert({itroot2->first, name1});
-                friend_n = relation.count(itroot2->first);
-
-            }
-            else { 
-                relation.insert({name1, name2});
-                relation.insert({name1, name1}); // root 
-                friend_n = 2;
-            }
-
-            printf("%d\n", friend_n);
-        }
+void union_find(int a, int b) {
+	a = find_root(a);
+	b = find_root(b);
+    
+    if( a != b ){
+		if (sizes[a] < sizes[b]) swap(a, b);
+		sizes[a] += sizes[b];
+		link[b] = a;
     }
+}
+
+int main() {
+
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int tetcase, relation_n, root1, root2;
+	cin >> tetcase;
+
+	string name1, name2;
+
+	for (int i=0; i<tetcase; i++) {
+		cin >> relation_n;
+
+		for (int j=0; j<MAX_SIZE; j++) {sizes[j] = 1; link[j] = j;}
+		map<string, int> hash_map;
+		int friend_n = 1;
+
+		for (int j=0; j<relation_n; j++) {
+			cin >> name1 >> name2;
+
+			if (hash_map.count(name1) == 0) hash_map[name1] = friend_n++;		
+			if (hash_map.count(name2) == 0) hash_map[name2] = friend_n++;
+			
+            union_find(hash_map[name1], hash_map[name2]);
+            
+            root1 = find_root(hash_map[name1]);
+            root2 = find_root(hash_map[name2]);
+            
+            if(sizes[root1] > sizes[root2]) {cout << sizes[root1] << endl;}
+            else {cout << sizes[root2] << endl;}
+		}
+	}
 }
